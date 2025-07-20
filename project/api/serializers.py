3aa67814fc4335961b0363c173ecc23673580b45
melissa.mangione@gmail.com <melissa.mangione@gmail.com>
2025-07-20 20:57:41 +0200

    validation simple serializer

--- project/api/serializers.py
+++ project/api/serializers.py
@@ -4,12 +4,27 @@ from .models import Category
 
 class ArticleSerializer(serializers.ModelSerializer):
     """
-    Sérialiseur pour convertir les objets Article en JSON.
+    Serializer pour le modèle Article incluant des validations simples.
     """
     class Meta:
         model = Article
-        fields = ['id', 'title', 'author', 'content', 'created_at']  # Champs à inclure dans le JSON
+        fields = ['id', 'title', 'content', 'created_at']
 
+    def validate_title(self, value):
+        """
+        Vérifie que le titre comporte au moins 5 caractères.
+        """
+        if len(value) < 5:
+            raise serializers.ValidationError("Le titre doit contenir au moins 5 caractères.")
+        return value
+
+    def validate_content(self, value):
+        """
+        Vérifie que le contenu n'est pas vide ou composé uniquement d'espaces.
+        """
+        if not value.strip():
+            raise serializers.ValidationError("Le contenu ne peut pas être vide.")
+        return value
 class CategorySerializer(serializers.ModelSerializer):
     """
     Sérialiseur pour convertir les objets Category en JSON.
